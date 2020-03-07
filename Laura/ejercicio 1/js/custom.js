b780a25f30e7a02a31439850ae09065ca76569ae
lau5 <55633315+lau5@users.noreply.github.com>
2020-03-07 14:17:22 -0600

    Clase 2
    Ejercicio 2

---
+++ Laura/ejercicio 1/js/custom.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+  slider();
+  colorFondo();
+
+  // console.log("Scripts OK.");
+});
+
+function colorFondo() {
+  let body = document.querySelector("body");
+
+  body.style.backgroundColor = "lime";
+}
+
+const slider = () => {
+  $("#bienvenida").slick({
+    autoplay: true,
+    autoplaySpeed: 1000,
+    speed: 1000,
+    dots: true
+  });
+};
