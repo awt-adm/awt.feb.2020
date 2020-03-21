bb8848a3b1978e9ae57f240ddbf6065894adb103
adrianamarin <marinady9@gmail.com>
2020-03-21 12:27:16 -0600

    21 de marzo

--- Adriana/ejercicios/ejercicio.dom/js/ejercicios.js
+++ Adriana/ejercicios/ejercicio.dom/js/ejercicios.js
@@ -2,14 +2,56 @@
 document.addEventListener("DOMContentLoaded", () => {
   operacion();
   textoBoton();
+
+  //Accede a objetos del dom
+  let h1 = document.querySelector("#calculos h1");
+  let h2 = document.querySelector("#calculos h2");
+
+  console.log("OBJETO: ", h1);
+
+  setTimeout(() => {
+    // Detiene la ejecución por un tiempo
+
+    h1.innerText = "Ya paso 1 segundos";
+  }, 1000);
+
+  let tiempo = 1;
+  setInterval(() => {
+    // Repite la ejecución por un tiempo determinado
+
+    h2.innerText = tiempo;
+    // h2.style.color = "purple"
+
+    tiempo++; // suma 1
+  }, 1000);
+  // fin accesde a elementos del dom
 });
 // Eventos
 // https://www.w3schools.com/js/js_events.asp
 // https://www.w3schools.com/jsref/dom_obj_event.asp
+//callback -> función que se ejecuta al finalizar una operación
+// Arrow Function -> funciones anónimas  = () => {}
 
-// variable global va fuera de una funciones
-let global = 10;
-let boton = document.querySelector("#suma");
+// Arrays o Lista
+// Son un grupo de objetos, elementos, valores, numeros, strings, funciones, etc....
+// se declaran dentro de corchetes -> [1,2,3,4,5,6,...]
+// Su indice se cuenta desde 0
+let array = [];
+console.log("Arreglo vacio: ", array);
+setTimeout(() => {
+  array.push("Instituto"); //agrega al array
+  array.push("ADM"); //agrega al array
+  array.push(["Array", "de", "Arrays"]);
+  // console.log("Array de arrays: ", array);
+
+  // Como acceder a los arrays
+  // Se hace por medio del índice con corchetes ej. [1] => segundo elemento
+  console.log("Accede 1: ", array[2]);
+  console.log("Accede 2: ", array[2][1]);
+}, 3000);
+
+// variable global va fuera de una función
+let boton = document.querySelector("#calcula");
 // operaciones
 const suma = (arg1 = 1, arg2 = 1) => {
   let resultado;
@@ -18,7 +60,7 @@ const suma = (arg1 = 1, arg2 = 1) => {
 
   producto(resultado);
 };
-
+// argumentos o parametros
 const multiplicacion = (arg1 = 1, arg2 = 1) => {
   let resultado;
 
